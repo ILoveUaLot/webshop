229a3e4319fbf9c199069abdfbee41e31e8ac2bd
Dmitriy Listopad <dimalist2005@gmail.com>
2024-02-03 23:33:10 +0300

    add home component

---
+++ StoreFrontend/src/app/pages/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {MatSidenavModule} from '@angular/material/sidenav'
+import {ProductsHeaderComponent} from './components/products-header/products-header.component'
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    MatSidenavModule,
+    ProductsHeaderComponent
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
