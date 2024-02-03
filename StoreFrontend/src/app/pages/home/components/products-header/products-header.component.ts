db83faa5ae0aa2825fcb8f0e74d150964f43cfc2
Dmitriy Listopad <dimalist2005@gmail.com>
2024-02-04 00:19:27 +0300

    add sorted button

--- StoreFrontend/src/app/pages/home/components/products-header/products-header.component.ts
+++ StoreFrontend/src/app/pages/home/components/products-header/products-header.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card'
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-products-header',
   standalone: true,
   imports: [
-    MatCardModule
+    MatCardModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule
   ],
   templateUrl: './products-header.component.html',
   styleUrl: './products-header.component.css'
 })
 export class ProductsHeaderComponent {
+  sort = "desc";
+  itemsShowCount = 12;
+  onSortUpdated(newSort:string): void
+  {
+    this.sort = newSort;
+  }
 
+  onItemsUpdated(count: number): void
+  {
+    this.itemsShowCount = count;
+  }
 }
